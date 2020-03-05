2ed11c190e9ccea479d75ef1c5751b63c1f0b61f
German Arcila <germanarcila@gmail.com>
2020-03-04 21:26:30 -0500

    cleared readme

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,63 @@
-const Twitter = require('twitter');
-require('dotenv').config();
+const axios = require('axios');
+const data = require('../public/assets/data.js').data;
+const ObjectsToCsv = require('objects-to-csv');
 
-const client = new Twitter({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret,
-});
-
-const setParams = param => {
-  return {screen_name: param, count: 100, tweet_mode: 'extended'};
-};
+const URLOne = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=';
+const URLTwo =
+  '&inputtype=textquery&fields=place_id,photos,formatted_address,name,rating,opening_hours,geometry&key=';
+const Api_key = process.env.API_KEY;
 
 module.exports = function(app) {
-  let extractedText = {user1: '', user2: ''};
-  app.get('/api/tweets/:user1/:user2', async function(req, res) {
-    await client.get(
-      'statuses/user_timeline',
-      setParams(req.params.user1),
-      function(error, tweets, response) {
-        if (!error) {
-          let textArr = tweets.map(tweet => tweet.full_text);
-          let text = '';
-          textArr.forEach(item => (text += ` ${item}`));
-          let replace = req.params.user1;
-          let re = new RegExp(replace, 'g');
-          extractedText.user1 = text.toLowerCase().replace(re, '');
-        }
-      }
-    );
-    await client.get(
-      'statuses/user_timeline',
-      setParams(req.params.user2),
-      function(error, tweets, response) {
-        if (!error) {
-          let textArr = tweets.map(tweet => tweet.full_text);
-          let text = '';
-          textArr.forEach(item => (text += ` ${item}`));
-          let replace1 = req.params.user2;
-          let re1 = new RegExp(replace1, 'g');
-          extractedText.user2 = text.toLowerCase().replace(re1, '');
-          res.json(extractedText);
-        }
-      }
-    );
-  });
+  getLibrariesMainInfo();
+};
+
+const getLibrariesMainInfo = function() {
+  // promise.all in order to be able to map over all elements in the data array
+  Promise.all(
+    data.map(library => {
+      const googleEndpoint = `${URLOne}${library}${URLTwo}${process.env.API_KEY}`;
+      return axios(googleEndpoint).then(data => data.data.candidates);
+    })
+
+    // once the promise is done get the returned object, flatten it (contains arrays within arrays)
+    // and pass it to the getSecondaryInfo function
+  ).then(libraryObj => getSecondaryInfo(libraryObj.flat()));
+};
+
+const getSecondaryInfo = branches => {
+  const urlFirst = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=';
+  const urlSecond = '&fields=name,rating,formatted_phone_number,website,url&key=';
+
+  // promise.all in order to be able to map over all elements in the data array
+  Promise.all(
+    branches.map(branch => {
+      return axios(`${urlFirst}${branch.place_id}${urlSecond}${Api_key}`).then(info => {
+        let libraryInformation = {
+          name: branch.name,
+          address: branch.formatted_address,
+          phone: info.data.result.formatted_phone_number,
+          website: info.data.result.website,
+          GoogleMaps: info.data.result.url,
+          postalCode: getPostal(branch.formatted_address),
+        };
+        return libraryInformation;
+      });
+    })
+  ).then(fullInformation => writeToCSV(fullInformation));
+};
+
+const getPostal = address => {
+  const postalCodeAndProvince = address.match(/ON .{3}\s.{3}/);
+  const postalCode =
+    (postalCodeAndProvince && postalCodeAndProvince[0].replace(/ON/, '')) || 'N/A';
+
+  //TODO:improve slice in order to get rid of the blank space at the beginning
+  return postalCode.slice(1);
+};
+
+const writeToCSV = arr => {
+  const target = './list.csv';
+  const csv = new ObjectsToCsv(arr);
+  csv.toDisk(target, {append: false});
+  console.log(`Done writting ✍️  to '${target}'`);
 };
